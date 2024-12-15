0e1234372703cf008d2b46f4504465add26ec69c
Praveen Shinde <shindepraveen002@gmail.com>
2024-12-15 11:22:25 +0530

    feat: added search funcnality

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,28 @@
+"use client";
 import LinkCard from "./components/link-card";
 import { links } from "./utils/data/data";
 import SearchBar from "./components/search-bar";
+import { useMemo, useState } from "react";
 export default function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const filteredLinks = useMemo(() => {
+    return links.filter((link) => {
+      const searchContent =
+        `${link.title} ${link.category} ${link.subcategory} `.toLowerCase();
+      return searchContent.includes(searchQuery.toLowerCase());
+    });
+  }, [searchQuery]);
+
+  const handleSearch = (query: string) => {
+    setSearchQuery(query);
+  };
+
   return (
     <div className="animate-slideUp">
-      <SearchBar />
-      <div className="grid grid-cols-1 gap-4 my-4 lg:grid-cols-3 overflow-y-scroll h-[calc(100vh-9rem)]">
-        {links.map((link) => (
+      <SearchBar onSearch={handleSearch} />
+      <div className="grid grid-cols-1 gap-4 my-4 lg:grid-cols-3 overflow-y-auto max-h-[calc(100vh-9rem)]">
+        {filteredLinks.map((link) => (
           <LinkCard key={link.title} {...link} />
         ))}
       </div>
