a737389427aa6fdefe3ef17e641c9d9e86fa238c
Praveen Shinde <shindepraveen002@gmail.com>
2024-12-17 22:50:14 +0530

    fix:Added debounce on search bar

--- app/categories/[category]/page.tsx
+++ app/categories/[category]/page.tsx
@@ -46,7 +46,7 @@ const Page = () => {
         </Link>
         <p className="capitalize font-medium">{category}</p>
       </div>
-      <SearchBar onSearch={handleSearch} />
+      <SearchBar onSearch={handleSearch} initialQuery={searchQuery} />
       {filteredLinks.length > 0 ? (
         <div className="grid grid-cols-1 gap-4 py-4 md:grid-cols-2 xl:grid-cols-3 overflow-y-auto max-h-[calc(100vh-11rem)] sm:max-h-[calc(100vh-9rem)] [&::-webkit-scrollbar]:hidden">
           {filteredLinks.map((link) => (
--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
 
   return (
     <div className="animate-slideUp">
-      <SearchBar onSearch={handleSearch} />
+      <SearchBar onSearch={handleSearch} initialQuery={searchQuery} />
       {filteredLinks.length > 0 ? (
         <div className="grid grid-cols-1 gap-4 py-4 md:grid-cols-2 xl:grid-cols-3 overflow-y-auto max-h-[calc(100vh-11rem)] sm:max-h-[calc(100vh-9rem)] [&::-webkit-scrollbar]:hidden">
           {filteredLinks.map((link) => (
