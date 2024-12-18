4ec370fcdd785f3637add8dd845117e0b6136ff4
Praveen Shinde <shindepraveen002@gmail.com>
2024-12-18 10:41:38 +0530

    fix: added suspense in searchbar

--- app/components/search-bar.tsx
+++ app/components/search-bar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Suspense } from "react";
 import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -35,15 +36,17 @@ export default function SearchBar({ onSearch, initialQuery }: SearchBarProps) {
   }, [debouncedSearchQuery, onSearch, router, searchParams]);
 
   return (
-    <div className="relative">
-      <MagnifyingGlassIcon className="absolute w-4 h-4 text-gray-400 transform -translate-y-1/2 left-4 top-1/2" />
-      <input
-        type="search"
-        placeholder="Search Links..."
-        className="w-full px-4 py-2 pl-10 text-sm border rounded-lg focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent"
-        onChange={handleInputChange}
-        value={searchQuery}
-      />
-    </div>
+    <Suspense>
+      <div className="relative">
+        <MagnifyingGlassIcon className="absolute w-4 h-4 text-gray-400 transform -translate-y-1/2 left-4 top-1/2" />
+        <input
+          type="search"
+          placeholder="Search Links..."
+          className="w-full px-4 py-2 pl-10 text-sm border rounded-lg focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent"
+          onChange={handleInputChange}
+          value={searchQuery}
+        />
+      </div>
+    </Suspense>
   );
 }
